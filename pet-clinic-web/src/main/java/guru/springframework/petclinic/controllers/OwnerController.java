598f0a77d3fce6a47bee69154956092f2bfe7fe2
abdulrahman.mian <abdulr.mian@gmail.com>
2021-03-22 17:46:21 +0100

    List all Owners on Owners Index Page. closes #11

--- pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/OwnerController.java
@@ -1,15 +1,30 @@
 package guru.springframework.petclinic.controllers;
 
+import guru.springframework.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/owners")
 @Controller
 public class OwnerController     {
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+
     //@RequestMapping({ "/owners","/owners/index","owners/index.html"})
     @RequestMapping({ "","/","/index","/index.html"})
     //Since owners is used in every quote, it can also be stated above in order to not replicate it
     // at the requestmapping above the method.
-    public String listOwners(){
+
+
+
+
+    public String listOwners(Model model){
+        model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
     }
 }
