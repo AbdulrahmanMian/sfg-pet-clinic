eaecd6814d3f063a0445f812aeac68a80c1c922f
abdulrahman.mian <abdulr.mian@gmail.com>
2021-06-14 15:32:07 +0200

    Added base class. closes #21

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/BaseEntity.java
@@ -1,8 +1,18 @@
 package guru.springframework.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     //But what we prefer to do is use box types and that is actually a recommendation of Hibernate to use
     //box types being that box types can in fact be null
